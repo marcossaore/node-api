02e93a7e66f94508adac42f6356957e814359f23
Marcos Soares <marcossaore@gmail.com>
2021-02-05 09:04:44 -0300

    feat: ensure EmailValidation returns a InvalidParamError when invalid email is provided

--- src/presentation/validation/email-validation.spec.ts
+++ src/presentation/validation/email-validation.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from '../errors'
 import { EmailValidator } from '../protocols/email-validator'
 import { EmailValidation } from './email-validation'
 
@@ -16,5 +17,21 @@ describe('Email Validation', () => {
     }
     sut.validate(data)
     expect(isValidSpy).toHaveBeenLastCalledWith('any_email@email.com')
+  })
+
+  test('should return a InvalidParamError when an invalid email is provided', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid (email: string): boolean {
+        return true
+      }
+    }
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut = new EmailValidation('email', emailValidatorStub)
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const data = {
+      email: 'invalid@email.com'
+    }
+    const error = sut.validate(data)
+    expect(error).toEqual(new InvalidParamError('email'))
   })
 })
