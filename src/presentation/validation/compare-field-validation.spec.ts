bb77f43fd90494e3a0d96e4b1cabed3b1ae17d07
Marcos Soares <marcossaore@gmail.com>
2021-02-04 21:28:16 -0300

    test: ensure CompareFieldValidation campare fields correctly

---
+++ src/presentation/validation/compare-field-validation.spec.ts
@@ -0,0 +1,24 @@
+import { InvalidParamError } from '../errors'
+import { CompareFieldValidation } from './compare-field-validation'
+
+describe('Compare Field Validation', () => {
+  test('should return a InvalidParamError if comparable fields to be diferents', () => {
+    const data = {
+      pass: 'value',
+      confpass: 'diferent_value'
+    }
+    const sut = new CompareFieldValidation('pass', 'confpass')
+    const error = sut.validate(data)
+    expect(error).toEqual(new InvalidParamError('confpass'))
+  })
+
+  test('should not return when comparable fields to be equals', () => {
+    const data = {
+      pass: 'value',
+      confpass: 'value'
+    }
+    const sut = new CompareFieldValidation('pass', 'confpass')
+    const error = sut.validate(data)
+    expect(error).toBeFalsy()
+  })
+})
