8536d979d1ae9195f863e461b0140cc94c23bab3
Marcos Soares <marcossaore@gmail.com>
2021-02-07 20:09:10 -0300

    feat: ensure SignupControllerFactory receives a DbVerifyAccount

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,4 +1,5 @@
 
+import { DbVerifyAccount } from '../../../data/usecases/verifiy-account/db-verify-account'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
@@ -14,7 +15,8 @@ export const makeSignupController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const validationComposite = makeSignupValidation()
-  const signupController = new SignupController(validationComposite, dbAddAccount)
+  const dbVerifyAccount = new DbVerifyAccount(accountMongoRepository)
+  const signupController = new SignupController(validationComposite, dbAddAccount, dbVerifyAccount)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signupController, logMongoRepository)
 }
