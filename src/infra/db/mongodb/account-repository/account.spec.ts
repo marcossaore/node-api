9c50734b7af5d70be09774e365ec3f9ab22e1466
Marcos Soares <marcossaore@gmail.com>
2021-02-07 20:04:14 -0300

    test: ensure AccountMongoRepository verify an email correctly

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,7 +1,17 @@
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 
+const makeAccount = (): AddAccountModel => ({
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
 describe('Account Mongo Repository', () => {
+  let accountCollections: Collection
+
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -11,7 +21,7 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollections = await MongoHelper.getCollection('accounts')
+    accountCollections = await MongoHelper.getCollection('accounts')
     await accountCollections.deleteMany({})
   })
 
@@ -21,11 +31,7 @@ describe('Account Mongo Repository', () => {
 
   test('Should return an account on success', async () => {
     const sut = makeSut()
-    const account = await sut.add({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
-    })
+    const account = await sut.add(makeAccount())
 
     expect(account).toBeTruthy()
 
@@ -33,5 +39,23 @@ describe('Account Mongo Repository', () => {
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
+  })
+
+  test('should return an account if email provided exists on db', async () => {
+    await accountCollections.insertOne(makeAccount())
+
+    const sut = makeSut()
+    const account = await sut.verify('any_email@mail.com')
+
+    expect(account).toBeTruthy()
+
+    expect(account.id).toBeTruthy()
+    expect(account.email).toBe('any_email@mail.com')
+  })
+
+  test('should return null if email provided no exists on db', async () => {
+    const sut = makeSut()
+    const account = await sut.verify('any_email@mail.com')
+    expect(account).toBeNull()
   })
 })
