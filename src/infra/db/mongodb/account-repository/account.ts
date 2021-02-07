919a0171a53043a258aa76e01cb60cd4fbcf8b0d
Marcos Soares <marcossaore@gmail.com>
2021-02-07 20:03:35 -0300

    feat: ensure AccountMongoRepository verify an email correctly

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,11 +2,18 @@ import { AddAccountRepository } from '../../../../data/protocols/db/add-account-
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { AccountModel } from '../../../../domain/models/account'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { VerifyExistedAccountRepository } from 'data/protocols/db/verify-existed-account-repository'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, VerifyExistedAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async verify (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const result = await accountCollection.findOne({ email })
+    return result ? MongoHelper.map(result) : null
   }
 }
