1d06b9315ca25b3c3916bf03b517cbc50c6a7d09
Marcos Soares <marcossaore@gmail.com>
2021-02-07 19:23:49 -0300

    refactor: move duplicated code to sut helper methods

--- src/data/usecases/verifiy-account/db-verify-account.spec.ts
+++ src/data/usecases/verifiy-account/db-verify-account.spec.ts
@@ -2,21 +2,39 @@ import { VerifyExistedAccountRepository } from '../../protocols/db/verify-existe
 import { AccountModel } from '../add-account/db-add-account-protocols'
 import { DbVerifyAccount } from './db-verify-account'
 
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email',
+  password: 'any_password'
+})
+
+const makeVerifyExistedAccountRepository = (): VerifyExistedAccountRepository => {
+  class DbVerifyAccountRepositoryStub implements VerifyExistedAccountRepository {
+    async verify (email: string): Promise<AccountModel> {
+      return makeFakeAccount()
+    }
+  }
+  return new DbVerifyAccountRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbVerifyAccount
+  dbVerifyAccountRepositoryStub: VerifyExistedAccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const dbVerifyAccountRepositoryStub = makeVerifyExistedAccountRepository()
+  const sut = new DbVerifyAccount(dbVerifyAccountRepositoryStub)
+  return {
+    sut,
+    dbVerifyAccountRepositoryStub
+  }
+}
+
 describe('DbVerifyAccount UseCase', () => {
   test('should call VerifyExistedAccountRepository with correct email', async () => {
-    class DbVerifyAccountRepositoryStub implements VerifyExistedAccountRepository {
-      async verify (email: string): Promise<AccountModel> {
-        const fakeAccount: AccountModel = {
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email',
-          password: 'any_password'
-        }
-        return fakeAccount
-      }
-    }
-    const dbVerifyAccountRepositoryStub = new DbVerifyAccountRepositoryStub()
-    const sut = new DbVerifyAccount(dbVerifyAccountRepositoryStub)
+    const { sut, dbVerifyAccountRepositoryStub } = makeSut()
     const verifySpy = jest.spyOn(dbVerifyAccountRepositoryStub, 'verify')
     await sut.verify('any_email')
     expect(verifySpy).toHaveBeenCalledWith('any_email')
