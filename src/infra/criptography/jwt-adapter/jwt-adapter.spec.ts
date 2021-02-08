14c34fc573c42affd12f7246610cde89a9c1bd20
Marcos Soares <marcossaore@gmail.com>
2021-02-08 19:56:18 -0300

    test: ensure JwtAdapter calls sign with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,12 +1,31 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  sign: async (): Promise<string> => {
+    return Promise.resolve('any_token')
+  }
+}))
+
 describe('JWT Adapter', () => {
   test('should call sign with correct values', async () => {
     const secret = 'secret'
     const sut = new JwtAdapter(secret)
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, secret, null, null)
+
+    let expectInFirstParam = signSpy.mock.calls[0][0]
+    expectInFirstParam = JSON.stringify(expectInFirstParam)
+    expect(expectInFirstParam).toBe(JSON.stringify({ id: 'any_id' }))
+
+    const expectInSecondParam = signSpy.mock.calls[0][1]
+    expect(expectInSecondParam).toBe(secret)
+  })
+
+  test('should return a token on sign success', async () => {
+    const secret = 'secret'
+    const sut = new JwtAdapter(secret)
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
   })
 })
