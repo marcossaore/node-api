7547469dbabb0feb36620b32dc78c4025ff7a693
Marcos Soares <marcossaore@gmail.com>
2021-02-08 15:30:17 -0300

    refactor: rename Encrypter to Hasher

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -16,20 +16,20 @@ describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.encrypt('valid_password')
+    await sut.hash('valid_password')
     expect(hashSpy).toHaveBeenCalledWith('valid_password', salt)
   })
 
   test('Should return a hash on success', async () => {
     const sut = makeSut()
-    const hash = await sut.encrypt('valid_password')
+    const hash = await sut.hash('valid_password')
     expect(hash).toBe('hash')
   })
 
   test('Should throw if bcrypt throws', async () => {
     const sut = makeSut()
     jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(Promise.reject(new Error()))
-    const promise = sut.encrypt('valid_password')
+    const promise = sut.hash('valid_password')
     await expect(promise).rejects.toThrow()
   })
 })
--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,14 +1,14 @@
-import { Encrypter } from '../../data/protocols/criptography/encrypter'
+import { Hasher } from '../../data/protocols/criptography/hasher'
 import bcrypt = require('bcrypt')
 
-export class BcryptAdapter implements Encrypter {
+export class BcryptAdapter implements Hasher {
   private readonly salt: number
 
   constructor (salt: number) {
     this.salt = salt
   }
 
-  async encrypt (value: string): Promise<string> {
+  async hash (value: string): Promise<string> {
     const hashed = bcrypt.hash(value, this.salt)
     return hashed
   }
