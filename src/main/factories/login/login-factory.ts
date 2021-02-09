059428c578b5f1a4be982688879c68d49e68ac22
Marcos Soares <marcossaore@gmail.com>
2021-02-09 19:38:12 -0300

    feat: add makeLoginFactory

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -5,13 +5,17 @@ import { DbAuthentication } from '../../../data/usecases/authentication/db-authe
 import { LoginController } from '../../../presentation/controllers/login/login-controller'
 import { Controller } from '../../../presentation/protocols'
 import { makeLoginValidation } from './login-validation-factory'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
+import env from '../../config/env'
 
 export const makeLoginController = (): Controller => {
   const salt = 12
-  const secret = 'secret'
   const accountMongoRepository = new AccountMongoRepository()
   const hashComparer = new BcryptAdapter(salt)
-  const encrypter = new JwtAdapter(secret)
+  const encrypter = new JwtAdapter(env.secret)
   const authentication = new DbAuthentication(accountMongoRepository, hashComparer, encrypter, accountMongoRepository)
-  return new LoginController(makeLoginValidation(), authentication)
+  const logMongoRepository = new LogMongoRepository()
+  const loginController = new LoginController(makeLoginValidation(), authentication)
+  return new LogControllerDecorator(loginController, logMongoRepository)
 }
