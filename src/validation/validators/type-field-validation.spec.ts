ccd2b64fa32574ef5edab32733d3aebaa55d1d7b
Marcos Soares <marcossaore@gmail.com>
2021-02-25 12:20:43 -0300

    test: ensure TypeFieldValidation traits array

--- src/validation/validators/type-field-validation.spec.ts
+++ src/validation/validators/type-field-validation.spec.ts
@@ -11,13 +11,22 @@ describe('Required Field Validation', () => {
     expect(error).toEqual(new TypeParamError('field', 'boolean'))
   })
 
-  test('should return a TypeParamError if field is string an the value provided is boolean', () => {
+  test('should return a TypeParamError if field is string and the value provided is boolean', () => {
     const data = {
       field: true
     }
     const sut = new TypeFieldValidation('field', 'string')
     const error = sut.validate(data)
     expect(error).toEqual(new TypeParamError('field', 'string'))
+  })
+
+  test('should return a TypeParamError if field is array and the value provided is boolean', () => {
+    const data = {
+      field: true
+    }
+    const sut = new TypeFieldValidation('field', 'array')
+    const error = sut.validate(data)
+    expect(error).toEqual(new TypeParamError('field', 'array'))
   })
 
   test('should not return on success', () => {
