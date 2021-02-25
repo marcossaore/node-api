e5bfc0b19425396817f905a9ca49d628c3a43856
Marcos Soares <marcossaore@gmail.com>
2021-02-25 12:46:33 -0300

    test: ensure NoAllowEmptyArrayValidation traits all scenarios

---
+++ src/validation/validators/no-allow-empty-array-validation.spec.ts
@@ -0,0 +1,22 @@
+import { NoAllowEmptyArrayError } from '../../presentation/errors'
+import { NoAllowEmptyArrayValidation } from './no-allow-empty-array-validation'
+
+describe('NoAllowEmptyArray Validation', () => {
+  test('should return NoAllowEmptyArrayError if empty array is provided', () => {
+    const data = {
+      arrayField: []
+    }
+    const sut = new NoAllowEmptyArrayValidation('arrayField')
+    const error = sut.validate(data)
+    expect(error).toEqual(new NoAllowEmptyArrayError('arrayField'))
+  })
+
+  test('should return NoAllowEmptyArrayError if null is provided as array', () => {
+    const data = {
+      arrayField: null
+    }
+    const sut = new NoAllowEmptyArrayValidation('arrayField')
+    const error = sut.validate(data)
+    expect(error).toEqual(new NoAllowEmptyArrayError('arrayField'))
+  })
+})
