b45d7e046753ad051b9d6bf70bce24bc7a09a4e9
Marcos Soares <marcossaore@gmail.com>
2021-02-24 23:00:11 -0300

    test: ensure AddSurveyModelValidator returns a TypeErrorParam if anwer has an invalid type

--- src/validation/model-validators/add-survey-model-validator.spec.ts
+++ src/validation/model-validators/add-survey-model-validator.spec.ts
@@ -37,4 +37,23 @@ describe('AddSurvey Validator', () => {
     const error = sut.validate(surveyDataWithoutParamsInQuestion)
     expect(error).toEqual(new MissingParamError('answers[1].answer'))
   })
+
+  test('should returns TypeErrorParam if "answers[X].answer" has an invalid type', () => {
+    const sut = new AddSurveyModelValidator()
+    const surveyDataWithInvalidParamsInQuestion = {
+      question: 'Any question',
+      answers: [
+        {
+          answer: 'any_answer'
+        },
+        {
+          answer: {
+            data: 'any_data'
+          }
+        }
+      ]
+    }
+    const error = sut.validate(surveyDataWithInvalidParamsInQuestion)
+    expect(error).toEqual(new TypeParamError('answers[1].answer', 'string'))
+  })
 })
