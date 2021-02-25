76ebe4801a611b21425006fe6797bdc487156da5
Marcos Soares <marcossaore@gmail.com>
2021-02-25 11:13:53 -0300

    feat: ensure TypeFieldValidation traits data and behaviors correctly

---
+++ src/validation/validators/type-field.validation.ts
@@ -0,0 +1,21 @@
+import { TypeParamError } from '../../presentation/errors'
+import { Validation } from '../../presentation/protocols'
+
+type TypeExpected = 'string' | 'number' | 'boolean' | 'undefined' | 'object' | 'function'
+
+export class TypeFieldValidation implements Validation {
+  private readonly fieldName: string
+  private readonly typeExpected: TypeExpected
+
+  constructor (fieldName: string, typeExpected: TypeExpected) {
+    this.fieldName = fieldName
+    this.typeExpected = typeExpected
+  }
+
+  validate (params: any): Error {
+    // eslint-disable-next-line valid-typeof
+    if (typeof params[this.fieldName] !== this.typeExpected) {
+      return new TypeParamError(this.fieldName, this.typeExpected)
+    }
+  }
+}
