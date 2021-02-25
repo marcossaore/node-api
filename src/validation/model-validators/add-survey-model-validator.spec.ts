1019d7a2251515929077a9793e562be28d76fc28
Marcos Soares <marcossaore@gmail.com>
2021-02-24 22:42:58 -0300

    test: ensure AddSurveyModelValidator returns a MissingParamError if answeer in array are no provided

--- src/validation/model-validators/add-survey-model-validator.spec.ts
+++ src/validation/model-validators/add-survey-model-validator.spec.ts
@@ -1,5 +1,5 @@
 import { AddSurveyModelValidator } from './add-survey-model-validator'
-import { TypeParamError } from '../../presentation/errors'
+import { MissingParamError, TypeParamError } from '../../presentation/errors'
 
 describe('AddSurvey Validator', () => {
   test('should returns TypeErrorParam if "answers" to be invalid in model', () => {
@@ -10,5 +10,21 @@ describe('AddSurvey Validator', () => {
     }
     const error = sut.validate(surveyDateWithoutAnswers)
     expect(error).toEqual(new TypeParamError('answers', 'array'))
+  })
+
+  test('should returns MissingParamError if "answers[X].answer" is no provided', () => {
+    const sut = new AddSurveyModelValidator()
+    const surveyDataWithoutParamsInQuestion = {
+      question: 'Any question',
+      answers: [
+        {
+          answer: 'any_answer'
+        },
+        {
+        }
+      ]
+    }
+    const error = sut.validate(surveyDataWithoutParamsInQuestion)
+    expect(error).toEqual(new MissingParamError('answers[1].answer'))
   })
 })
