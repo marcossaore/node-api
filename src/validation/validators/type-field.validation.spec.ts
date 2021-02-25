2aeaa8be5604d30ddab20e0c0da0fb4fe739de93
Marcos Soares <marcossaore@gmail.com>
2021-02-25 11:14:41 -0300

    test: ensure TypeFieldValidation traits data and behaviors correctly

---
+++ src/validation/validators/type-field.validation.spec.ts
@@ -0,0 +1,31 @@
+import { TypeParamError } from '../../presentation/errors'
+import { TypeFieldValidation } from './type-field.validation'
+
+describe('Required Field Validation', () => {
+  test('should return a TypeParamError if field is boolean and the value provided is string', () => {
+    const data = {
+      field: 'any_value'
+    }
+    const sut = new TypeFieldValidation('field', 'boolean')
+    const error = sut.validate(data)
+    expect(error).toEqual(new TypeParamError('field', 'boolean'))
+  })
+
+  test('should return a TypeParamError if field is string an the value provided is boolean', () => {
+    const data = {
+      field: true
+    }
+    const sut = new TypeFieldValidation('field', 'string')
+    const error = sut.validate(data)
+    expect(error).toEqual(new TypeParamError('field', 'string'))
+  })
+
+  test('should not return on success', () => {
+    const data = {
+      field: 'any_value'
+    }
+    const sut = new TypeFieldValidation('field', 'string')
+    const error = sut.validate(data)
+    expect(error).toBeFalsy()
+  })
+})
