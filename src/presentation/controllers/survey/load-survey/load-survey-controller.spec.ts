08d452956839c5c590de53cc94b7bc95de6a85b3
Marcos Soares <marcossaore@gmail.com>
2021-03-17 13:21:17 -0300

    test: ensure  LoadSurveyController returns 204 if no exists surveys

--- src/presentation/controllers/survey/load-survey/load-survey-controller.spec.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.spec.ts
@@ -1,6 +1,7 @@
 
 import { SurveyModel, LoadSurvey, HttpRequest } from './load-survey-controller-protocols'
 import { LoadSurveyController } from './load-survey-controller'
+import { noContent } from '../../../helpers/http/http-helpers'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: null,
@@ -48,5 +49,12 @@ describe('LoadSurvey Controller', () => {
     const loadSpy = jest.spyOn(loadSurveyStub, 'load')
     await sut.handle(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('should return 204 if no exists surveys', async () => {
+    const { sut, loadSurveyStub } = makeSut()
+    jest.spyOn(loadSurveyStub, 'load').mockReturnValueOnce(null)
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
