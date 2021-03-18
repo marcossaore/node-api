c951fcfdc6ef1e21e3aac771ceda8ab9e438c23b
Marcos Soares <marcossaore@gmail.com>
2021-03-18 17:07:31 -0300

    test: ensure SaveSurveyResultController calls LoadSurveyById with correct surveyId

--- src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.spec.ts
@@ -1,23 +1,73 @@
-import {} from './save-survey-result-controller-protocols'
+import { HttpRequest, LoadSurveyById } from './save-survey-result-controller-protocols'
 import { SaveSurveyResult } from './save-survey-result-controller'
 import { badRequest } from '@/presentation/helpers/http/http-helpers'
 import { MissingParamError } from '@/presentation/errors'
+import { SurveyModel } from '@/domain/models/survey'
+import MockDate from 'mockdate'
+
+const makeFakeRequest = (): HttpRequest => ({
+  parameters: {
+    surveyId: 'any_id'
+  }
+})
+
+const makeFakeSurveyModel = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [
+    {
+      answer: 'first_answer'
+    },
+    {
+      answer: 'second_answer'
+    }
+  ],
+  date: new Date()
+})
+
+const makeLoadSurveyById = (): LoadSurveyById => {
+  class LoadSurveyByIdStub implements LoadSurveyById {
+    async load (id: string): Promise<SurveyModel> {
+      return makeFakeSurveyModel()
+    }
+  }
+
+  return new LoadSurveyByIdStub()
+}
 
 type SutTypes = {
   sut: SaveSurveyResult
+  loadSurveyByIdStub: LoadSurveyById
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new SaveSurveyResult()
+  const loadSurveyByIdStub = makeLoadSurveyById()
+  const sut = new SaveSurveyResult(loadSurveyByIdStub)
   return {
-    sut
+    sut,
+    loadSurveyByIdStub
   }
 }
 
 describe('SaveSurveyResult Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   test('should return 400 if surveyId is no provided', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(badRequest(new MissingParamError('surveyId')))
+  })
+
+  test('should call LoadSurveyById with correct surveyId', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    const spyLoad = jest.spyOn(loadSurveyByIdStub, 'load')
+    await sut.handle(makeFakeRequest())
+    expect(spyLoad).toHaveBeenCalledWith('any_id')
   })
 })
