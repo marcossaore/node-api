9c739c41fc11ab58108c9b603ee3fc51d403dcd2
Marcos Soares <marcossaore@gmail.com>
2021-03-18 17:06:42 -0300

    feat: ensure SaveSurveyResultController calls LoadSurveyById with correct surveyId

--- src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.ts
@@ -1,11 +1,18 @@
 import { MissingParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers/http/http-helpers'
-import { Controller, HttpRequest, HttpResponse } from './save-survey-result-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResult implements Controller {
+  private readonly loadSurveyById: LoadSurveyById
+  constructor (loadSurveyById: LoadSurveyById) {
+    this.loadSurveyById = loadSurveyById
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.parameters?.surveyId) {
+    const surveyId = httpRequest.parameters?.surveyId
+    if (!surveyId) {
       return badRequest(new MissingParamError('surveyId'))
     }
+    await this.loadSurveyById.load(surveyId)
   }
 }
